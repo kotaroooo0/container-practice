19ab98f94f91cd266a4b46ed786c60b6aa6c6a02
kotaroooo0 <k33asby@gmail.com>
2020-06-21 21:58:52 +0900

    lb

--- service/aws_security_group.tf
+++ service/aws_security_group.tf
@@ -2,6 +2,16 @@ resource "aws_security_group" "instance" {
     name = "instance"
     description = "instance sg"
     vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
+
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        security_groups = [
+            aws_security_group.alb.id,
+        ]
+    }
+
     egress {
         from_port = 0
         to_port = 0
@@ -10,6 +20,24 @@ resource "aws_security_group" "instance" {
     }
 }
 
+resource "aws_security_group" "alb" {
+    name = "sample-rails-alb"
+    description = "http and https"
+    vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
 
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "TCP"
+        cidr_blocks = [
+            "0.0.0.0/0",
+        ]
+    }
 
-
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
